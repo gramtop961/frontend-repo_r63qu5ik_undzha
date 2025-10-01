f28d5477053dfa29ec2fec70b4d597985484bfae
gramtop961 <gramtop961@gmail.com>
2025-10-02 01:31:25 +0530

    Add .next/static/chunks/template_frontend_src_app_layout_tsx_61ff1781._.js from template

---
+++ .next/static/chunks/template_frontend_src_app_layout_tsx_61ff1781._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__0ec5cad6._.css",
+  "static/chunks/5c34e_next_dist_f0763fdd._.js"
+],
+    source: "dynamic"
+});
