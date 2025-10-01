bbb1f9569be49991c7e849268364822c6a90a9b1
gramtop961 <gramtop961@gmail.com>
2025-10-02 01:31:23 +0530

    Add .next/static/chunks/template_frontend_a0ff3932._.js from template

---
+++ .next/static/chunks/template_frontend_a0ff3932._.js
@@ -0,0 +1,13 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_9daca535._.js",
+  "static/chunks/5c34e_next_dist_compiled_react-dom_8ed7d9ee._.js",
+  "static/chunks/5c34e_next_dist_compiled_next-devtools_index_6b9faa97.js",
+  "static/chunks/5c34e_next_dist_compiled_726fee0f._.js",
+  "static/chunks/5c34e_next_dist_client_278fbc86._.js",
+  "static/chunks/5c34e_next_dist_bfaf049b._.js",
+  "static/chunks/5c34e_@swc_helpers_cjs_a56fcc62._.js"
+],
+    source: "entry"
+});
